7446dd80364d03296598e10026d38c9eabf49554
masahiro96848 <feetwcpic9436@gmail.com>
2024-05-24 10:57:43 +0900

    usersテーブルを作成。devise_token_auth周りのファイルを作成

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   namespace :api do
     namespace :v1 do
       get 'health_check', to: 'health_check#index'
+      mount_devise_token_auth_for 'User', at: 'auth'
     end
   end
 end
