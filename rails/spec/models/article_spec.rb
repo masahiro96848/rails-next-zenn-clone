03e2fdfc00b73f726c8b8447eb8203c2d5ad1673
masahiro96848 <feetwcpic9436@gmail.com>
2024-05-24 13:32:28 +0900

    rubocopでリファクタリング

--- rails/spec/models/article_spec.rb
+++ rails/spec/models/article_spec.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  context "factoryのデフォルト設定に従った時" do
-    subject {create(:article)}
+  context 'factoryのデフォルト設定に従った時' do
+    subject { create(:article) }
 
-    it "正常レコードを新規作成できる" do
-      expect { subject }.to change { Article.count}.by(1)
+    it '正常レコードを新規作成できる' do
+      expect { subject }.to change { Article.count }.by(1)
     end
   end
 
-  describe "Validaiotns" do
-    subject {article.valid?}
+  describe 'Validaiotns' do
+    subject { article.valid? }
 
     let(:article) { build(:article, title:, content:, status:, user:) }
     let(:title) { Faker::Lorem.sentence }
@@ -18,35 +20,35 @@ RSpec.describe Article, type: :model do
     let(:status) { :published }
     let(:user) { create(:user) }
 
-    context "全ての値が正常な時" do
-      it "検証が通る" do
+    context '全ての値が正常な時' do
+      it '検証が通る' do
         expect(subject).to be_truthy
       end
     end
 
-    context "ステータスが公開済みかつ、タイトルが空の時"  do
-      let(:title) {""}
+    context 'ステータスが公開済みかつ、タイトルが空の時' do
+      let(:title) { '' }
 
-      it "エラーメッセージが返る" do
+      it 'エラーメッセージが返る' do
         expect(subject).to be_falsy
-        expect(article.errors.full_messages).to eq ["タイトルを入力してください"]
+        expect(article.errors.full_messages).to eq ['タイトルを入力してください']
       end
     end
 
-    context "ステータスが公開済みかつ、本文が空の時" do
-      let(:content) { "" }
+    context 'ステータスが公開済みかつ、本文が空の時' do
+      let(:content) { '' }
 
-      it "エラーメッセージが返る" do
+      it 'エラーメッセージが返る' do
         expect(subject).to be_falsy
-        expect(article.errors.full_messages).to eq ["本文を入力してください"]
+        expect(article.errors.full_messages).to eq ['本文を入力してください']
       end
     end
 
-     context "ステータスが未保存かつ、すでに同一ユーザーが未保存ステータスの記事を所有していた時" do
+    context 'ステータスが未保存かつ、すでに同一ユーザーが未保存ステータスの記事を所有していた時' do
       let(:status) { :unsaved }
       before { create(:article, status: :unsaved, user:) }
 
-      it "例外が発生する" do
+      it '例外が発生する' do
         expect { subject }.to raise_error(StandardError)
       end
     end
