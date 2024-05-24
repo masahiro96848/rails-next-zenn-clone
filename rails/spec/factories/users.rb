4fb43986afcd70a3571435e24e515f9d3d238f35
masahiro96848 <feetwcpic9436@gmail.com>
2024-05-24 12:39:39 +0900

    テストコードを追加

---
+++ rails/spec/factories/users.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :user do
+    name { '田中太郎' }
+    sequence(:email) { |n| "#{n}_test@example.com" }
+    password { 'password' }
+    confirmed_at { Time.current }
+  end
+end
