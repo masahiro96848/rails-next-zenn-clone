83c2761d3b2a2a5692683ce2a3e1d0f49de9af7b
masahiro96848 <feetwcpic9436@gmail.com>
2024-05-24 15:15:58 +0900

    記事一覧APIを実装

--- rails/app/controllers/api/v1/articles_controller.rb
+++ rails/app/controllers/api/v1/articles_controller.rb
@@ -3,6 +3,11 @@
 module Api
   module V1
     class ArticlesController < ApplicationController
+      def index
+        articles = Article.published.order(created_at: :desc).page(params[:page] || 1).per(10)
+        render json: articles
+      end
+
       def show
         article = Article.published.find(params[:id])
         render json: article
