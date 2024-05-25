b09bf067037fdd480d1811cf91c24c5d27f19ada
masahiro96848 <feetwcpic9436@gmail.com>
2024-05-25 11:34:00 +0900

    認証リンクの修正。

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       get 'health_check', to: 'health_check#index'
       mount_devise_token_auth_for 'User', at: 'auth'
+      namespace :user do
+        resource :confirmations, only: [:update]
+      end
 
       namespace :current do
         resource :user, only: %i[show]
